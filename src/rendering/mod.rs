83969b94943ff7562a8f1707df75283eee455234
Ben Merritt <blm768@gmail.com>
2018-10-30 22:23:31 -0700

    Fix order of color components

--- src/rendering/mod.rs
+++ src/rendering/mod.rs
@@ -12,9 +12,9 @@ pub type Rgb = RGB<f32>;
 pub type Rgba = RGBA<f32>;
 
 fn rgb_as_vec3(rgb: &Rgb) -> Vector3<f32> {
-    Vector3::new(rgb.r, rgb.b, rgb.g)
+    Vector3::new(rgb.r, rgb.g, rgb.b)
 }
 
 fn rgba_as_vec4(rgba: &Rgba) -> Vector4<f32> {
-    Vector4::new(rgba.r, rgba.b, rgba.g, rgba.a)
+    Vector4::new(rgba.r, rgba.g, rgba.b, rgba.a)
 }
